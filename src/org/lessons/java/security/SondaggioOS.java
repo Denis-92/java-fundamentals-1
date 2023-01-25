3a07a3037026c2d093764337df487ea2cc847553
Denis-92 <d.mozenic@gmail.com>
2023-01-25 14:51:56 +0100

    init esercizio Sondaggio OS

---
+++ src/org/lessons/java/security/SondaggioOS.java
@@ -0,0 +1,15 @@
+package org.lessons.java.security;
+
+public class SondaggioOS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Facciamo un sondaggio tra gli studenti di quale sistema operativo usano e popoliamo delle variabili.
+		
+		// Usiamo le variabili per fare semplici calcoli (es. percentuale)
+		// 		eventualmente applicando casting tra int e double.
+
+	}
+
+}
