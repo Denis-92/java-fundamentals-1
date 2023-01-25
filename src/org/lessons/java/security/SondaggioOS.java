6fb5acb117abb06b82c5ffba59e8fc4a51e1f4b7
Denis-92 <d.mozenic@gmail.com>
2023-01-25 15:55:14 +0100

    add variables, inputs and calculate sum total users

--- src/org/lessons/java/security/SondaggioOS.java
+++ src/org/lessons/java/security/SondaggioOS.java
@@ -1,5 +1,7 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class SondaggioOS {
 
 	public static void main(String[] args) {
@@ -9,6 +11,28 @@ public class SondaggioOS {
 		
 		// Usiamo le variabili per fare semplici calcoli (es. percentuale)
 		// 		eventualmente applicando casting tra int e double.
+		
+		System.out.println("Benvenuto, questo è un sondaggio per misurare l'utilizzo degli OS.");
+		
+		// Aperto inputContainer
+		Scanner inputContainer = new Scanner(System.in);
+		
+		System.out.print("Quanti utenti utilizzano Windows? ");
+		int usersWindows = inputContainer.nextInt();
+		
+		System.out.print("Quanti utenti utilizzano Linux? ");
+		int usersLinux = inputContainer.nextInt();
+		
+		System.out.print("Quanti utenti utilizzano Mac? ");
+		int usersMac = inputContainer.nextInt();
+		
+		inputContainer.close();
+		// Chiuso inputContainer
+		
+		int totalUsers = usersWindows + usersLinux + usersMac;
+		
+		
+		
 
 	}
 
