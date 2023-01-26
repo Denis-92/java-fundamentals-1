22b06db17b8713ae20a4a12b642250826a7632c2
Denis-92 <d.mozenic@gmail.com>
2023-01-26 14:21:01 +0100

    init Cibi Preferiti

---
+++ src/org/lessons/java/food/CibiPreferiti.java
@@ -0,0 +1,18 @@
+package org.lessons.java.food;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+		// Nel programma inizializzate un array (con i metodi visti a lezione)
+		// con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi)
+		// Poi stampate a video:
+		// 		- la lunghezza della classifica
+		//		- il vostro cibo top (prima posizione della classifica)
+		//		- il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+		// Le modalità di consegna sono le solite:
+		//		repo GitHub, file .gitignore, commit e push
+		// BONUS Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana
+
+	}
+
+}
