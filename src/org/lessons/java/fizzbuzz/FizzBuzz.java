4034ae55e4ae0665042350cac2dca3783ee02204
Denis-92 <d.mozenic@gmail.com>
2023-01-26 16:05:03 +0100

    add exercise FizzBuzz

--- src/org/lessons/java/fizzbuzz/FizzBuzz.java
+++ src/org/lessons/java/fizzbuzz/FizzBuzz.java
@@ -7,6 +7,38 @@ public class FizzBuzz {
 		//		ma per i multipli di 3 stampi “Fizz” al posto del numero
 		//		e per i multipli di 5 stampi Buzz.
 		// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+		
+			
+		for (int i = 1; i <= 100; i++ ) {
+			
+			boolean Fizz = i % 3 == 0;
+			
+			boolean Buzz = i % 5 == 0;
+			
+			if (Fizz && Buzz) {
+				
+				System.out.println("FizzBuzz");
+				
+			} else {
+				
+				if (Fizz) {
+					
+					System.out.println("Fizz");
+					
+				} else {
+					
+					if (Buzz) {
+				
+						System.out.println("Buzz");
+					
+					} else {
+					
+						System.out.println(i);
+						
+					}
+				}	
+			}
+		}
 
 	}
 
